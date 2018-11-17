cf2d076900f39048f8b8ab3a0490a13f76e4d70e
mohuang <mayerui@live.com>
2018-11-18 03:13:18 +0800

    解决换行符干扰

--- cluster/substopword/substopword.py
+++ cluster/substopword/substopword.py
@@ -3,7 +3,7 @@ import sys
 file_stopword = open('stopword.txt', 'r', encoding='UTF-8')
 stopword = list()
 for line in file_stopword:
-    stopword.append(line)
+    stopword.append(line.strip())
 file_stopword.close()
 
 sys.argv.append('result.txt')
@@ -13,7 +13,18 @@ lines = f.readlines()
 f.close()
 
 f = open(sys.argv[1], 'w+', encoding='UTF-8')
+
 for line in lines:
-    if line not in stopword:
-        f.write(line)
+    line = line.strip()
+    words = line.split(' ')
+    _words = []
+    for i in range(0, len(words)):
+        word = words[i]
+        if word not in stopword:
+            _words.append(word)
+
+    line = ' '.join(_words)+'\n'
+
+    f.write(line)
+
 f.close()
